2136889e135863da1e8171a4ad15b8874f586127
oommaarrmmoohhaammeedd <mahmodkhaled30000@gmail.com>
2024-08-06 14:40:36 +0300

    update

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { ContactComponent } from './contact/contact.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+export const routes: Routes = [
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
+    { path: "about", component: AboutComponent },
+    { path: "portfolio", component: GalleryComponent },
+    { path: "contact", component: ContactComponent },
+];
